ce8d6079210666f535b375d4c8db2cb04ec79c96
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-08 10:33:45 +0530

    UC2 Chhose your choice

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -7,11 +7,37 @@ namespace _07_Workshop
     public class TicTacToeGame
     {
         char[] board = new char[10];
+        char humanChoice = ' ', computerChoice = ' ';
+
         public TicTacToeGame()
         {
             for(int i = 0; i < board.Length; i++)
             {
                 board[i] = ' ';
+            }
+            ChoiceOfLetter();
+        }
+
+        public void ChoiceOfLetter()
+        {
+            Console.WriteLine("Please enter your choice!");
+            Console.WriteLine("Enter 1 for X \nEnter 2 for O");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    humanChoice = 'X';
+                    computerChoice = 'O';
+                    break;
+                case "2":
+                    humanChoice = 'O';
+                    computerChoice = 'X';
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input, default choice X assigned to user!");
+                    humanChoice = 'X';
+                    computerChoice = 'O';
+                    break;
             }
         }
 
