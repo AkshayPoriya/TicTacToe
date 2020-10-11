170efbfed4f69f9467dde506490814a3b04faa1b
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-11 16:32:55 +0530

    [Akshay].[Refactor].Bot become little smart, can play moves which leads to win

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -15,7 +15,7 @@ namespace _07_Workshop
         // Initialize new instance of TicTacToeGame, default constructor
         public TicTacToeGame()
         {
-            for(int i = 0; i < board.Length; i++)
+            for (int i = 0; i < board.Length; i++)
             {
                 board[i] = ' ';
                 positionFilled[i] = false;
@@ -27,67 +27,82 @@ namespace _07_Workshop
         // Toss to elect who moves first
         private void Toss()
         {
-            Console.WriteLine("\n********************** TOSS **********************");
-            string userCoinChoice;
-            Console.WriteLine("Enter your choice for Toss!");
-            Console.WriteLine("Press 1 for Head \nPress 2 for Tail");
-            string option = Console.ReadLine();
-            switch (option)
+            try
             {
-                case "1":
-                    userCoinChoice = "Head";
-                    break;
-                case "2":
-                    userCoinChoice = "Tail";
-                    break;
-                default:
-                    Console.WriteLine("Wrong Input, default choice Head assigned to user!");
-                    userCoinChoice = "Head";
-                    break;
+                Console.WriteLine("\n********************** TOSS **********************");
+                string userCoinChoice;
+                Console.WriteLine("Enter your choice for Toss!");
+                Console.WriteLine("Press 1 for Head \nPress 2 for Tail");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        userCoinChoice = "Head";
+                        break;
+                    case "2":
+                        userCoinChoice = "Tail";
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input, default choice Head assigned to user!");
+                        userCoinChoice = "Head";
+                        break;
+                }
+                int tossIndex = random.Next(0, 2);
+                string[] tossResult = { "Head", "Tail" };
+                Console.WriteLine("{0} it is!", tossResult[tossIndex]);
+                if (tossResult[tossIndex] == userCoinChoice)
+                {
+                    Console.WriteLine("User Wins the Toss\n---------------");
+                    Console.WriteLine("What would you like! Move First or Second");
+                    Console.WriteLine("Press 1 for First Move \nPress 2 for Second Move");
+                    string moveChoice = Console.ReadLine();
+                    movesFirst = (moveChoice == "1") ? "User" : "Bot";
+                }
+                else
+                {
+                    Console.WriteLine("Bot Wins the Toss");
+                    movesFirst = "Bot";
+                }
             }
-            int tossIndex = random.Next(0, 2);
-            string[] tossResult = { "Head", "Tail" };
-            Console.WriteLine("{0} it is!",tossResult[tossIndex]);
-            if (tossResult[tossIndex] == userCoinChoice)
+            catch
             {
-                Console.WriteLine("User Wins the Toss\n---------------");
-                Console.WriteLine("What would you like! Move First or Second");
-                Console.WriteLine("Press 1 for First Move \nPress 2 for Second Move");
-                string moveChoice = Console.ReadLine();
-                movesFirst = (moveChoice == "1") ? "User" : "Bot";
-            }
-            else
-            {
-                Console.WriteLine("Bot Wins the Toss");
-                movesFirst = "Bot";
+                Console.WriteLine("Something Went Wrong, Please Try Again!");
+                Toss();
             }
         }
 
         // Choice out of X and O
         private void ChoiceOfLetter()
         {
-            Console.WriteLine("\n************ Select you Choice: X or O ************");
-            Console.WriteLine("Please enter your choice!");
-            Console.WriteLine("Enter 1 for X \nEnter 2 for O");
-            string option = Console.ReadLine();
-            switch (option)
-            {
-                case "1":
-                    humanChoice = 'X';
-                    computerChoice = 'O';
-                    break;
-                case "2":
-                    humanChoice = 'O';
-                    computerChoice = 'X';
-                    break;
-                default:
-                    Console.WriteLine("Wrong Input, default choice X assigned to user!");
-                    humanChoice = 'X';
-                    computerChoice = 'O';
-                    break;
+            try {
+                Console.WriteLine("\n************ Select you Choice: X or O ************");
+                Console.WriteLine("Please enter your choice!");
+                Console.WriteLine("Enter 1 for X \nEnter 2 for O");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        humanChoice = 'X';
+                        computerChoice = 'O';
+                        break;
+                    case "2":
+                        humanChoice = 'O';
+                        computerChoice = 'X';
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input, default choice X assigned to user!");
+                        humanChoice = 'X';
+                        computerChoice = 'O';
+                        break;
+                }
+                Console.WriteLine("User Choice: " + humanChoice);
+                Console.WriteLine("Bot Choice: " + computerChoice);
             }
-            Console.WriteLine("User Choice: "+humanChoice);
-            Console.WriteLine("Bot Choice: "+computerChoice);
+            catch
+            {
+                Console.WriteLine("Something Went Wrong, Please Try Again!");
+                ChoiceOfLetter();
+            }
         }
 
         // Just to demonstrate rules and positions of the board
@@ -109,7 +124,7 @@ namespace _07_Workshop
         private void DisplayBoard()
         {
             Console.WriteLine("\n     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1],board[2],board[3]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
@@ -173,39 +188,47 @@ namespace _07_Workshop
         // To take input from user to play its move
         private void UserMove()
         {
-            if (!PositionAvailable())
-                return;
-            bool flag = true;
-            int userMove=0;
-            while (flag)
+            try
             {
-                Console.WriteLine("Available Positions in Board:");
-                for (int i = 1; i < positionFilled.Length; i++)
+                if (!PositionAvailable())
+                    return;
+                bool flag = true;
+                int userMove = 0;
+                while (flag)
                 {
-                    if (!positionFilled[i])
+                    Console.WriteLine("Available Positions in Board:");
+                    for (int i = 1; i < positionFilled.Length; i++)
                     {
-                        Console.Write(i + " ");
+                        if (!positionFilled[i])
+                        {
+                            Console.Write(i + " ");
+                        }
+                    }
+                    Console.WriteLine("\nPlease enter your choice from above-mentioned choices");
+                    userMove = Convert.ToInt32(Console.ReadLine());
+                    if (userMove >= 1 && userMove <= 9 && positionFilled[userMove])
+                    {
+                        Console.WriteLine("Invalid Choice, Position already filled! Enter again!");
+                    }
+                    else if (userMove < 1 || userMove > 9)
+                    {
+                        Console.WriteLine("Invalid Entry! Try Again!");
+                    }
+                    else
+                    {
+                        board[userMove] = humanChoice;
+                        positionFilled[userMove] = true;
+                        flag = false;
                     }
                 }
-                Console.WriteLine("\nPlease enter your choice from above-mentioned choices");
-                userMove = Convert.ToInt32(Console.ReadLine());
-                if (userMove >= 1 && userMove <= 9 && positionFilled[userMove])
-                {
-                    Console.WriteLine("Invalid Choice, Position already filled! Enter again!");
-                }
-                else if (userMove < 1 || userMove > 9)
-                {
-                    Console.WriteLine("Invalid Entry! Try Again!");
-                }
-                else
-                {
-                    board[userMove] = humanChoice;
-                    positionFilled[userMove] = true;
-                    flag = false;
-                }
+                Console.WriteLine("\nUser marks at: " + userMove);
+                DisplayBoard();
             }
-            Console.WriteLine("\nUser marks at: " + userMove);
-            DisplayBoard();
+            catch
+            {
+                Console.WriteLine("Something Went Wrong, Please Try Again!");
+                UserMove();
+            }
         }
 
         // Rules for Bot to play game
@@ -213,6 +236,23 @@ namespace _07_Workshop
         {
             if (!PositionAvailable())
                 return;
+            for (int i = 1; i < positionFilled.Length; i++)
+            {
+                if (!positionFilled[i])
+                {
+                    board[i] = computerChoice;
+                    if (FindWinner() == "Bot")
+                    {
+                        positionFilled[i] = true;
+                        Console.WriteLine("Bot marks at: " + i);
+                        DisplayBoard();
+                        return;
+                    }
+                    else
+                        board[i] = ' ';
+                }   
+            }
+
             List<int> availablePlaces = new List<int>();
             for (int i = 1; i < positionFilled.Length; i++)
             {
@@ -221,12 +261,11 @@ namespace _07_Workshop
                     availablePlaces.Add(i);
                 }
             }
-            Random random = new Random();
             int cursorMovement = random.Next(0, availablePlaces.Count);
             int botMove = availablePlaces[cursorMovement];
             board[botMove] = computerChoice;
             positionFilled[botMove] = true;
-            Console.WriteLine("Bot marks at: "+botMove);
+            Console.WriteLine("Bot marks at: " + botMove);
             DisplayBoard();
         }
 
@@ -239,12 +278,17 @@ namespace _07_Workshop
                 string result = FindWinner();
                 if (result == "Bot")
                 {
-                    Console.WriteLine("Bot Wins the Game");
+                    Console.WriteLine("Bot Wins the Game!");
                     return;
                 }
                 if (result == "User")
                 {
-                    Console.WriteLine("User Wins the Game");
+                    Console.WriteLine("User Wins the Game!");
+                    return;
+                }
+                if (result == "Tie")
+                {
+                    Console.WriteLine("Game Tie!");
                     return;
                 }
                 BotMove();
@@ -268,8 +312,8 @@ namespace _07_Workshop
             Toss();
             ChoiceOfLetter();
             DisplayBoardInitial();
-            Console.WriteLine("***************** Lets Play Game *****************");
-            if(movesFirst == "Bot")
+            Console.WriteLine("\n***************** Lets Play Game *****************");
+            if (movesFirst == "Bot")
             {
                 BotMove();
                 PlayTillEnd();
@@ -281,4 +325,6 @@ namespace _07_Workshop
         }
 
     }
+
+
 }
